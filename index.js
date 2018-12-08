671e3a0b93958412ff89336bfbc675994c6271e6
ghostbyte <asyrafcloud@gmail.com>
2018-12-08 09:49:14 +0800

    Add files

---
+++ index.js
@@ -0,0 +1,33 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const player = require('node-wav-player');
+
+async function monitor() {
+    let ticketAvailable = false;
+	while(true) {
+		await rp('https://ticket.tickethotline.com.my/')
+			.then(async (data) => {
+				const str = cheerio('[id=bodyHolder_ctl00_pnlBuy]', data).text();
+				if(!str.includes('Our online tickets have reached its limit')) {
+					console.log('Tiket dah ada la kot.');
+					ticketAvailable = true;
+					//console.log(`https://ticket.tickethotline.com.my/${cheerio('[id=bodyHolder_ctl00_pnlBuy] > a', data).attr.href}`);
+				}
+				if(str.includes('Our online tickets have reached its limit')) {
+					console.log('Ticket belum ada lagi.');
+				}
+				if(data.includes('The service is unavailable.')) {
+					console.log('Tidak pasti, mungkin laman sesawang x hidup.');
+				}
+			})
+			.catch(err => err);
+		
+		if(ticketAvailable === true) {
+			await player.play({ path: './alarm.wav', sync: true })
+				.then(() => console.log('Alarm dah bunyi.'))
+				.catch(error => console.error(error));
+		}
+	}
+};
+
+monitor();
